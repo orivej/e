7f785a0b075668879332ded9538efa9b75cad85f
Orivej Desh <orivej@gmx.fr>
2015-10-18 21:13:29 +0000

    Initial commit.

---
+++ error.go
@@ -0,0 +1,40 @@
+package e
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+)
+
+func context() string {
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return ""
+	}
+	fn := runtime.FuncForPC(pc)
+	return fmt.Sprintf("%s (%s:%d)", fn.Name(), path.Base(file), line)
+}
+
+func Exit(e error) {
+	if e != nil {
+		fmt.Printf("%s: %s\n", context(), e)
+		os.Exit(1)
+	}
+}
+
+func Print(e error) bool {
+	if e != nil {
+		fmt.Printf("%s: %s\n", context(), e)
+	}
+	return e != nil
+}
+
+func CloseOrExit(c io.Closer) {
+	Exit(c.Close())
+}
+
+func CloseOrPrint(c io.Closer) {
+	Print(c.Close())
+}
